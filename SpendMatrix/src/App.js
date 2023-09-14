54f568e0231c71e60ff2478d9e1a75a239b0c931
mayurisamanta <samantamayuri@gmail.com>
2023-09-14 15:56:00 +0530

    adding ExpenseDate component

--- SpendMatrix/src/App.js
+++ SpendMatrix/src/App.js
@@ -9,7 +9,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -28,9 +33,10 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        // expense={expenses[0]} alternative
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
