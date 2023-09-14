eecdafeb6d30ba49b23bd6307e8f11799922ed98
mayurisamanta <samantamayuri@gmail.com>
2023-09-14 16:04:39 +0530

    adding Expenses Component

--- SpendMatrix/src/App.js
+++ SpendMatrix/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -30,30 +31,7 @@ function App() {
   ];
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        // expense={expenses[0]} alternative
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
-    </div>
+    <Expenses expenses={expenses}/>
   );
 }
 
