f6d6db4cbb351729f2ad950cea333b61bcedb592
mayurisamanta <samantamayuri@gmail.com>
2023-10-03 15:50:26 +0530

    adding add new expense button

--- SpendMatrix/src/components/NewExpense/ExpenseForm.js
+++ SpendMatrix/src/components/NewExpense/ExpenseForm.js
@@ -28,45 +28,48 @@ const ExpenseForm = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={(event) => {
-              inputChangeHandler("title", event.target.value);
-            }}
-          />
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={(event) => {
+                inputChangeHandler("title", event.target.value);
+              }}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              value={enteredAmount}
+              onChange={(event) => {
+                inputChangeHandler("amount", event.target.value);
+              }}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              value={enteredDate}
+              min="2019-01-01"
+              max="2023-12-31"
+              onChange={(event) => {
+                inputChangeHandler("date", event.target.value);
+              }}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={enteredAmount}
-            onChange={(event) => {
-              inputChangeHandler("amount", event.target.value);
-            }}
-          />
+        <div className="new-expense__actions">
+          <button onClick={props.onCancel}>Cancel</button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            min="2019-01-01"
-            max="2023-12-31"
-            onChange={(event) => {
-              inputChangeHandler("date", event.target.value);
-            }}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
--- SpendMatrix/src/components/NewExpense/NewExpense.js
+++ SpendMatrix/src/components/NewExpense/NewExpense.js
@@ -1,18 +1,42 @@
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
 
+    setIsEditing(false);
     props.onAddExpense(expenseData);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  if (isEditing === false) {
+    return (
+      <div className="new-expense">
+        <button onClick={startEditingHandler}> Add New Expense</button>
+      </div>
+    );
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={stopEditingHandler}
+      />
     </div>
   );
 };
